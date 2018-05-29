90fde0a72d328f9543d944414e17d0bb0f5658f5
eacvn <39713183+eacvn@users.noreply.github.com>
2018-05-29 19:55:58 +0300

    add test

---
+++ TestTaskMindBoxTests/TriangleTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Geometry.Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {
+        [TestMethod()]
+        public void TriangleTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Triangle(-1, 1, 1));
+            Assert.ThrowsException<ArgumentException>(() => new Triangle(10, 1.99999999999, 12.0));
+
+        }
+
+        [TestMethod()]
+        public void CalcAreaTest()
+        {
+            Assert.AreEqual(new Triangle(3,4,5).CalcArea(),6.0,1e-7);
+        }
+
+        [TestMethod()]
+        public void IsRightTriangleTest()
+        {
+            Assert.IsTrue(new Triangle(3, 4, 5).IsRightTriangle());
+            Assert.IsFalse(new Triangle(3, 4, 4).IsRightTriangle());
+
+        }
+    }
+}
---
+++ TestTaskMindBoxTests/СircleTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Geometry.Tests
+{
+    [TestClass()]
+    public class СircleTests
+    {
+        [TestMethod()]
+        public void СircleTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Сircle(-1));
+
+        }
+
+        [TestMethod()]
+        public void CalcAreaTest()
+        {
+            Assert.AreEqual(new Сircle(1).CalcArea(), 3.1415926535897932384626433832795, 1e-7);
+        }
+    }
+}
